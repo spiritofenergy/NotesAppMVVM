8cf28ee9659ecba7c23972926fc0a2b5da8d224b
spiritofenrgy <spiritofenergy220@gmail.com>
2022-11-18 21:36:58 +0300

    Update 2

--- app/src/main/java/com/kodexcompany/notesappmvvm/database/firebase/AppFirebaseRepository.kt
+++ app/src/main/java/com/kodexcompany/notesappmvvm/database/firebase/AppFirebaseRepository.kt
@@ -37,8 +37,18 @@ class AppFirebaseRepository:  DatabaseRepository {
       }
 
       override suspend fun update(note: Note, onSuccess: () -> Unit) {
-        TODO("Not yet implemented")
-    }
+          val noteId = note.firebaseId
+          val mapNotes = hashMapOf<String, Any>()
+
+          mapNotes[FIREBASE_ID] = noteId
+          mapNotes[Constants.Keys.TITLE] = note.title
+          mapNotes[Constants.Keys.SUBTITLE] = note.subtitle
+
+          database.child(noteId)
+              .updateChildren(mapNotes)
+              .addOnSuccessListener { onSuccess() }
+              .addOnFailureListener { Log.d("checkData", "Failed to update note") }
+}
 
       override suspend fun delete(note: Note, onSuccess: () -> Unit) {
         TODO("Not yet implemented")
